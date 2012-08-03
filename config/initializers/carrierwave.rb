4f3039bd6e3309f312912d8407eccbad871e80db
Charlie Magee <charlie@signaldesign.net>
2012-08-03 14:18:02 -0700

    fixed typo in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
     #:region                 => 'us-standard'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'Gettinout'                     # required
+  config.fog_directory  = 'gettinout'                     # required
   # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
   # config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
