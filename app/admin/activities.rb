40fa8e624c95108e58afd168175cb08e0c82ae6e
Charlie Magee <charlie@signaldesign.net>
2012-09-17 21:42:05 -0700

    trying to get Event working

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Activity do
   
+  index do
+    
+    column :title
+    
+    column :location
+    column :created_at
+    default_actions
+  end
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Activity Details" do
+      f.input :image, :as => :file, :hint => f.template.image_tag(f.object.image_url(:thumb))
+      f.input :title
+      f.input :location
+    end
+    f.buttons
+  end
+
 end
--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Event do
-	
+
+	index do
+    column :date
+    column :created_at
+    default_actions
+  end
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Event Details" do      
+      f.input :activity, :as => :radio, :collection => Activity.all
+      f.input :date
+    end
+    f.buttons
+  end
 end
